e967500cbed1e7888a344c1ae8bba7256621b718
pcloverdev <pcloverdevelopment@outlook.com>
2022-10-24 18:10:50 -0600

    created a dummy controller for receiving image post requests that just returns an ok response

---
+++ ImageUploaderAPI/ImageUploaderAPI/Controllers/ImageUploadController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ImageUploaderAPI.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class ImageUploadController : ControllerBase
+  {
+    [HttpPost(Name = "UploadImage")]
+    public IActionResult Post([FromForm] IFormFile file)
+    {
+      string fileName = file.FileName;
+
+      // TODO: Change this to a 201 created, and probably return the URL to the image?
+      return Ok();
+    }
+  }
+}
